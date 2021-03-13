549994585a1e90acbb19e2086cd4302c30536854
Ahmad Hassan <ahmadh807@gmail.com>
2021-03-13 15:01:21 +0500

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react";
+import CurrencyRow from './CurrencyRow';
+const url = "https://api.exchangeratesapi.io/latest";
 function App() {
+  const [currency, setCurrency] = useState([]);
+  console.log(currency);
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setCurrency([data.base, ...Object.keys(data.rates)]);
+    })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>&#36; Convertor</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
     </div>
   );
 }
---
+++ src/CurrencyRow.js
@@ -0,0 +1,12 @@
+const CurrencyRow = () => {
+    return  (
+        <div>
+            <input type="number" className="inputField" />
+            <select>
+                <option value="Hey">Hey</option>
+            </select>
+        </div>
+    );
+}
+
+export default CurrencyRow;
