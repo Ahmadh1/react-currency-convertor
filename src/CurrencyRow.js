995262a8bedbf0f62eac5c70e81b8ac315c22bbc
Ahmad Hassan <ahmadh807@gmail.com>
2021-03-13 18:39:42 +0500

    Final push

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,12 +1,27 @@
-const CurrencyRow = () => {
-    return  (
-        <div>
-            <input type="number" className="inputField" />
-            <select>
-                <option value="Hey">Hey</option>
-            </select>
-        </div>
-    );
-}
+const CurrencyRow = ({
+  currencyOptions,
+  selectedCurrency,
+  onChangeCurrency,
+  onChangeAmount,
+  amount,
+}) => {
+  return (
+    <div>
+      <input
+        type="number"
+        className="inputField"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default CurrencyRow;
